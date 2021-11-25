147e2cee1afa07bb782d49e036366d5af7b44945
Haiol <lovehp30@gmail.com>
2021-11-25 20:29:21 +0900

    add catmoving add clockRandomGeneration

--- app/src/main/java/com/lovehp30/clockgame/StampActivity.java
+++ app/src/main/java/com/lovehp30/clockgame/StampActivity.java
@@ -2,13 +2,23 @@ package com.lovehp30.clockgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.lovehp30.clockgame.databinding.ActivityStampBinding;
+
 public class StampActivity extends AppCompatActivity {
+
+    ActivityStampBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stamp);
+        binding = ActivityStampBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnStampBack.setOnClickListener(v->{
+            startActivity(new Intent(getApplicationContext(),StartActivity.class));
+            finish();
+        });
     }
 }
